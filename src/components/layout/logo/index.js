65d1962de35806edcd5ec8b24084b4f4d09fa160
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-08-11 14:32:48 -0300

    images for logo

--- src/components/layout/logo/index.js
+++ src/components/layout/logo/index.js
@@ -1,7 +1,8 @@
-import { Fragment } from "react";
+import styles from "./logo.module.scss";
+import Image from "next/image";
 
-export default function Logo(){
-    return <Fragment>
-        
-    </Fragment>
+export default function Logo() {
+  return <div className={styles.logo}>Johnatan P</div>
+  //      -------------opção de logo-------------
+  //   <Image src="/image/John3.png" width={110} height={35} />
 }
