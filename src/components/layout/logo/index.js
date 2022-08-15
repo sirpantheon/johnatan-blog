b0fcb733364665d4be52a760999d23797fb7970a
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-08-15 00:18:48 -0300

    refatorando 2

--- src/components/layout/logo/index.js
+++
@@ -1,8 +0,0 @@
-import styles from "./logo.module.scss";
-import Image from "next/image";
-
-export default function Logo() {
-  return <div className={styles.logo}>Johnatan P</div>
-  //      -------------opção de logo-------------
-  //   <Image src="/image/John3.png" width={110} height={35} />
-}
