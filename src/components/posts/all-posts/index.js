29c1442e766f6226468a4979e328428f5f79c1b2
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-08-15 00:19:05 -0300

    refatorando 3

--- src/components/posts/all-posts/index.js
+++
@@ -1,9 +0,0 @@
-import PostsGrid from "../posts-grid";
-import styles from "./all-posts.module.scss";
-
-export default function AllPosts(props) {
-  return <section className={styles.posts}>
-    <h1>Todas Postagens</h1>
-    <PostsGrid posts={props.posts}/>
-  </section>;
-}
