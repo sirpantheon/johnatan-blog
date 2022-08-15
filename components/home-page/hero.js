9682b07d57a7a1a57dc686a0fe47d5cc4cc556f8
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-08-15 00:15:54 -0300

    finalizando 0.2

---
+++ components/home-page/hero.js
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+
+import classes from './hero.module.css';
+
+function Hero() {
+
+  const Imagen1 = '/images/site/Image-Johnatan.jpg'
+  const Imagen2 = '/images/site/Image-Johnatan.jpg'
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src='/images/site/john3.png'
+          alt='Eu sou o Johnatan'
+          width={250}
+          height={250}
+        />
+      </div>
+      <h1>Óla, Meu Nome e Johnatan</h1>
+      <p>
+        Este e o meu blog sobre a minha evolução como Web Developer - Especialidade em frameworks como NextsJs e React.
+      </p>
+      <p>NextsJs | React | TypeScript | NodeJs | MongoDb | Sass</p>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ components/layout/logo.js
@@ -0,0 +1,7 @@
+import classes from './logo.module.css';
+
+function Logo() {
+  return <div className={classes.logo}>Johnatan S S Paixão</div>;
+}
+
+export default Logo;
