ed033e042b0b1c9ee9c21f84204c86f60bf29889
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-08-16 21:36:23 -0300

    ajustando scss 2

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -1,10 +1,10 @@
 import PostsGrid from '../posts/posts-grid';
-import classes from './featured-posts.module.css';
+import classes from './featured-posts.module.scss';
 
 function FeaturedPosts(props) {
   return (
     <section className={classes.latest}>
-      <h2>Featured Posts</h2>
+      <h2>Posts em Destaques</h2>
       <PostsGrid posts={props.posts} />
     </section>
   );
